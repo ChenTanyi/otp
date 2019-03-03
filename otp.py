e37c63b69567057307893b8f0fa02be1fe3df7ce
ChenTanyi <chentanyi@gmail.com>
2019-03-03 18:45:12 +0800

    add otp in mac

---
+++ otp.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import os
+import sys
+import rumps
+import pyotp
+import logging
+import urllib.parse
+import pyperclip
+
+
+class OTPApp(rumps.App):
+
+    def __init__(self, otp, *args, **kwargs):
+        self._otp = otp
+        super().__init__(*args, **kwargs)
+
+    @rumps.clicked('OTP')
+    def otp(self, sender):
+        otp_str = self._otp.now()
+        logging.debug(f'Get OTP: {otp_str}')
+        pyperclip.copy(otp_str)
+
+
+def get_otp():
+    import qr
+    qr_code = qr.scan_qr('qr.png')[0]
+    url = urllib.parse.urlparse(qr_code.data.decode())
+    if url.scheme != 'otpauth' and url.netloc != 'totp':
+        logging.exception(
+            f'ERR! url scheme: {url.scheme} net loc: {url.netloc}')
+
+    query = urllib.parse.parse_qs(url.query)
+    if 'secret' not in query:
+        logging.exception('secret not exists')
+
+    secret = query['secret']
+    if isinstance(secret, list):
+        secret = secret[0]
+
+    otp = pyotp.TOTP(secret)
+    return otp
+
+
+def main():
+    otp = get_otp()
+    logging.debug(f'Test OTP: {otp.now()}')
+    OTPApp(otp, 'OTP').run()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level = logging.DEBUG, format = '%(asctime)s %(levelname)s %(message)s')
+    main()
---
+++ qr.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import re
+import zbar
+import requests
+from PIL import Image, ImageGrab
+import argparse
+from io import BytesIO
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url')
+
+    args = parser.parse_args()
+    return args
+
+
+def is_url(url):
+    return re.match(r'https?://', url) is not None
+
+
+def get_image(url):
+    if is_url(url):
+        r = requests.get(url)
+        r.raise_for_status()
+        return Image.open(BytesIO(r.content))
+    else:
+        return Image.open(url)
+
+
+def scan_qr(image):
+    if isinstance(image, str):
+        image = get_image(image)
+
+    assert isinstance(image, Image.Image)
+
+    gray = image.convert('L')
+
+    scanner = zbar.Scanner()
+    return scanner.scan(gray)
+
+
+def main():
+    args = arg_parse()
+    im = args.url
+    if not im:
+        im = ImageGrab.grabclipboard()
+
+    print(scan_qr(im))
+
+
+if __name__ == '__main__':
+    main()
